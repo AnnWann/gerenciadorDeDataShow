2aa631475855879b3f1d2ffab46e8f3fedf62f88
AnnWann <uanabgasparim@gmail.com>
2025-01-22 21:13:18 -0300

    fix: datashowController

--- src/gerenciadorDeDataShow/controller/datashowController.clj
+++ src/gerenciadorDeDataShow/controller/datashowController.clj
@@ -1,9 +1,20 @@
 (ns gerenciadorDeDataShow.controller.datashowController
   (:require
-    [gerenciadorDeDataShow.database.datashowDB :as datashowDB]))
+   [gerenciadorDeDataShow.database.datashowDB :as datashowDB]
+   [gerenciadorDeDataShow.view.view :as view]))
 
-(defn newDatashow []
-  (datashowDB/create-datashow))
+(defn new-datashow []
+  (let [id (datashowDB/create-datashow)]
+    (view/log "Datashow adicionado!")
+    (view/log (str "ID: " id))))
 
-(defn eraseDatashow [id]
-  (datashowDB/delete-datashow id))
+(defn get-all-datashows []
+  (let [datashows (datashowDB/read-all-datashows)]
+    (view/log "Listando datashows: ")
+    (doseq [datashow datashows]
+      (view/show-datashow datashow))))
+
+(defn erase-datashow [id]
+  (datashowDB/delete-datashow id)
+  (view/log "Datashow removido!"))
+
